26c553d8e0b821fd9643da9d85cee2889f285513
Zachary Salmon <zachary_salmon@brown.edu>
2013-12-01 16:44:25 -0500

    much of back end finished, although database is only populated with dummy values (unformated), still need to update job info

---
+++ loader.js
@@ -0,0 +1,15 @@
+var anyDB = require('any-db');
+var jobsConn = anyDB.createConnection('sqlite3://jobs.db');
+
+jobsConn.query('CREATE TABLE jobs (title TEXT, hours TEXT, rate TEXT)')
+	.on('end', function(){
+		console.log('Made table!');
+	});
+
+for (var i=0;i<20;i++){
+	var numericId = i.toString();
+	jobsConn.query('INSERT INTO jobs VALUES ($1, $2, $3)', ['Dummy Job Title' + numericId, 
+								'Dummy Hours ' + numericId, 'Dummy rate ' + numericId]).on('end', function(){
+									console.log('added a row')          
+								});
+}
--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 var express = require('express');
 var app = express();
 
+var anyDB = require('any-db');
+var jobsConn = anyDB.createConnection('sqlite3://jobs.db');
+
 var engines = require('consolidate');
 app.engine('html', engines.hogan);
-app.set('views', __dirname + '/templates');
+app.set('views', __dirname + "/templates");
+app.use('/public', express.static(__dirname + '/public'));
 
+app.get('/app', function(request, response){
+	//get information for top var in list. Populate variables based on that.
+	var jobsList = new Array();
+	var q = "SELECT * FROM jobs";
+	var i =0;
+	var allJobs = jobsConn.query(q);
+	allJobs.on('row', function(row){
+		jobsList[i] = row;
+		i++;
+	})
+	allJobs.on('end', function(){
+		var l = jobsList.length;
+		console.log(l);
+		var jobStr="";
+		for(var j=0; j<l; j++){
+			var row = jobsList[j];
+			jobStr+=row.title +row.hours + row.rate+"|";
+		}
+		response.render("standin.html", {variable: "Title", othVariable: "CONTENT", jobs: jobStr });
+	});
+	
+});
 
-app.get('/', function(request, response){
-	response.render("standin.html", {variable: "Title", othVariable: "CONTENT"});
-})
-
+/*app.get('/app/:jobId', function(request, response){
+	//get information for job Id, Populate variables based on that - no need to re render?
+});
+*/
 app.listen(8080, function(){
 	console.log('Server listening on port 8080');
 });
